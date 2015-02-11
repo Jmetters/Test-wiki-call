ba23e3f87df048b363067c138590dbb0a9f1b479
John Richardson <richardson@squirescompany.com>
2015-02-11 11:38:51 -0600

    Removing the event.

--- concho/js/site.js
+++ concho/js/site.js
@@ -899,7 +899,7 @@ var rc = {
 	debug: false,
 	
 	container: "#home-feeds .section-content",
-	navItems: "#home-feeds .section-nav .nav > li > a",
+	navItems: "#home-feeds .section-nav .nav > li",
 	mobileNav: "#home-feeds .section-nav #select1",
 	loading: "loading",
 	
