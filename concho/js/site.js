b833e11b52a0860974f67a6f96e882265f5f1458
Ben Harwell <harwell@squirescompany.com>
2015-02-19 11:58:57 -0600

    Restored autoHeight for proper mobile styling of the carousel.

--- concho/js/site.js
+++ concho/js/site.js
@@ -17,7 +17,7 @@ var homeFeaturedCarousel = {
 			var owl = $(homeFeaturedCarousel.slideshow).owlCarousel({
 					"items"	: 1,
 					"singleItem" : true,
-					"autoHeight" : false,
+					"autoHeight" : true,
 					"autoPlay" : 15000,
 					"transitionStyle" : "fade",
 					"stopOnHover": true,
